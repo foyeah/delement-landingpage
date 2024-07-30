a0bb498b2b25282edf53de78f47e9cb9df779763
Mikhail Foye <wasdbit@gmail.com>
2024-07-30 11:39:33 +0500

    1h 30m add fonts, fix small bugs

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
         ],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(png|svg|jpg|jpeg|gif|woff)$/i,
         type: 'asset/resource',
       },
     ],
